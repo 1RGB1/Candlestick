1c4806f5cdcb577c18b136c92d003100a7fd1403
Ahmad Ragab Muhammad <ahmad.ragab@ripple-innovations.com>
2022-07-19 23:34:35 +0200

    Initial Commit

---
+++ Candlestick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Candlestick
+//
+//  Created by Ahmad Ragab on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
